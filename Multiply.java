c583f5b0f29e79facf972dab050d7048c967af82
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-01-22 18:18:18 +0530

    Fourth change

--- Multiply.java
+++ Multiply.java
@@ -3,6 +3,8 @@ class Multiply{
     public static void main(String [] args){
         System.out.println(m(3,4));
     }
+    // this is the funtion to multiply and return the value
+
     static int m(int a, int b){
         return a*b;
     }
