ed45e1ada6ed029d52e7ea70616e58ad10dbe61f
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-01-22 18:09:43 +0530

    second change

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Multiply{
+    public static void main(String [] args){
+        System.out.println(m(3,4));
+    }
+    static int m(int a, int b){
+        return a*b;
+    }
+}
