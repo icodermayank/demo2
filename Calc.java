e248be895b8cd6d438e62879116cc973a2b0bab4
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-01-23 12:43:08 +0530

    Command line calc updated

--- Calc.java
+++ Calc.java
@@ -2,7 +2,27 @@ import java.util.Scanner;
 
 class Calc{
     public static void main(String [] args){
+        System.out.println("Welcome to the command line calculator.");
+        System.out.prinltn("Enter Your No.");
 
+        Scanner s = new Scanner (System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println("Press 1 for add\nPress 2 for substract\nPress 3 for Multiply\nPress 4 for divide");
+       
+        int c = s.nextInt();
+        
+        switch(c){
+            case 1: System.out.println(add(a,b));
+            break;
+            case 2: System.out.println(sub(a,b));
+            break;
+            case 3: System.out.println(mul(a,b));
+            break;
+            case 4 : System.out.println(div(a,b));
+            default : System.out.println("Invalid input please try again.");
+        }
 
     }
     static int add(int a, int b){
